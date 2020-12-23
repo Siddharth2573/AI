4ed5e86a40750ee60c94310778865404f30813bb
Siddharth2573 <56190581+Siddharth2573@users.noreply.github.com>
2020-12-23 19:43:52 +0530

    Add files via upload

---
+++ MM.py
@@ -0,0 +1,16 @@
+def maxminposition(A, n): 
+
+	minposition = A.index(min(A)) 
+ 
+	maxposition = A.index(max(A)) 
+	print ("The maximum is at position::", maxposition + 1) 
+	print ("The minimum is at position::", minposition + 1) 
+
+A=list() 
+n=int(input("Enter the size of the List ::")) 
+print("Enter the Element ::") 
+for i in range(int(n)): 
+	k=int(input("")) 
+	A.append(k) 
+maxminposition(A,n)
+
